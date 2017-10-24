6f4dabbb3c0545e842163dbb76a0d1748c6b367c
Gabriel <gabriel.favilla@cczcom.com.br>
2017-10-24 10:40:42 -0200

    Commit inicial

---
+++ assets/json/showroom.php
@@ -0,0 +1,31 @@
+<?php
+	
+header('Content-type: application/json');
+
+/**
+ * Include e Riquire CMS
+ */
+require_once('../../application/include_dao.php');
+
+//start new transaction
+$transaction = new Transaction();
+
+$veiculo = new Veiculo();
+
+$veiculo = DAOFactory::getVeiculoDAO()->relTable(' SELECT 
+													*,v.alias as alias,v.idAlbum as idAlbum,a.file as capa,c.nome as idCor,vc.alias as idVeiculoCategoria,m.nome as idMarca,m.idMarca as km 
+													FROM Veiculo v INNER JOIN albumMidia a ON (v.idAlbum=a.idAlbum) 
+													INNER JOIN Marca m ON (v.idMarca=m.idMarca)
+													INNER JOIN Cor c ON (c.idCor=v.idCor)
+													INNER JOIN veiculoCategoria vc ON (vc.idVeiculoCategoria=v.idVeiculoCategoria)
+													WHERE v.status>0 and a.ordem=0 GROUP BY a.idAlbum ORDER BY a.ordem ASC ');
+
+
+echo json_encode($veiculo);
+
+//commit transaction
+$transaction->commit();
+
+
+	
+?>
